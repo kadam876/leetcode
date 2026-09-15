a6d728323877d56e2e36ca6063e17caee9002d83
kadam876 <jgkadam370123@kkwagh.edu.in>
2026-09-15 23:31:28 +0530

    Time: 4 ms (14.05%), Space: 36.3 MB (18.94%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        vector<int> res;
-        res.resize(nums.size());
-        res[0]=nums[0];
-        int n= nums.size();
-        for(int i=1;i<nums.size();i++){
-            res[i]=res[i-1]+nums[i];
+        vector<int>sum(nums.size());
+        int j=0;
+        sum[0]=nums[0];
+        for (int i=1;i<nums.size();i++){
+            sum[i]=sum[i-1]+nums[i];
         }
-         for(int i=0;i<nums.size();i++){
-           int left_sum = (i == 0) ? 0 : res[i - 1];
-           int right_sum=res[n-1]-res[i];
-           if(left_sum==right_sum){return i;}
+        if(j==0){if(sum[sum.size()-1]-nums[0]==0)
+        return j;}
+        for(j=1;j<nums.size();j++){
+            if(sum[j-1]==sum[sum.size()-1]-sum[j])
+            return j;
         }
         return -1;
     }
